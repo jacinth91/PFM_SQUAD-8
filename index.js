eb2f8593a238092f76ceead727d0a6e44bd047a8
jamalm <jamaludheen.m@danubehome.com>
2025-06-28 15:20:27 +0400

    index

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 require("dotenv").config({ path: ".env" });
 const connectDB = require("./config/db");
 const express = require("express");
-const swaggerUi = require('swagger-ui-express');
-const swaggerSpecs = require('./config/swagger');
-const logger = require('./utils/logger');
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpecs = require("./config/swagger");
+const logger = require("./utils/logger");
 var cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-
 
 // Swagger documentation route
 app.use(
@@ -28,9 +27,8 @@ app.use("/api/private", require("./routes/private"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/products", require("./routes/products"));
 app.use("/api/transaction", require("./routes/transaction"));
-app.use('/api/orders', require("./routes/orders"));
-app.use('/api/account', require("./routes/account"));
-
+app.use("/api/orders", require("./routes/orders"));
+app.use("/api/account", require("./routes/account"));
 
 //server handler
 PORT = process.env.PORT || 5000;
@@ -45,15 +43,4 @@ const server = app.listen(PORT, () => {
   );
   //console.log("app listning on port", PORT);
   logger.info(`Server running on port ${PORT}`);
-});
-
-//utils
-app.use((err, req, res, next) => {
-  logger.error(err);
-  res.status(500).json({ error: "Internal server error" });
-});
-
-process.on("uncaughtException", (err) => {
-  logger.error("Uncaught Exception:", err);
-  // Optionally notify admin or restart logic
 });
