36e5e61e8c4e57fbc0bd3419c4e1081033f9489b
jamalm <jamaludheen.m@danubehome.com>
2025-06-28 13:03:57 +0400

    updated

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,20 @@
 const { User } = require("../models/User");
+const auditEmitter = require("../events/auditEvents");
 
 exports.register = async (req, res) => {
   try {
     let { FIRST_NAME, LAST_NAME, EMAIL_ADDRESS, password } = req.body;
     let user;
-        user = await User.create({
-          FIRST_NAME,
-          LAST_NAME,
-          EMAIL_ADDRESS,
-          password
-        });
-
+    user = await User.create({
+      FIRST_NAME,
+      LAST_NAME,
+      EMAIL_ADDRESS,
+      password,
+    });
+    auditEmitter.emit("user:register", {
+      idUserLoginDetail: user?._id,
+      sessionId: "N/A", // or null
+    });
     sendToken(user, 201, res);
   } catch (error) {
     console.log(error);
