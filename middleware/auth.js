bbd446f76b01644d2560370c8e2add6d3bb3a429
pinko42 <mohamed.abderrezague@gmail.com>
2025-06-28 13:11:41 +0400

    private route

---
+++ middleware/auth.js
@@ -0,0 +1,35 @@
+const jwt = require("jsonwebtoken");
+const { User } = require("../models/User");
+
+exports.protect = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    return res.status(401).json({ success: false, error: "Not authorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.id);
+
+    if (!user) {
+      return res.status(404).json({ success: false, error: "User not found" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: "Not authorized",
+    });
+  }
+};
